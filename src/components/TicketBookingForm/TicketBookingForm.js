ac695b7327ecc611e170ad4d774488427ddafd92
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-17 20:24:23 +0800

    Add submit button which makes POST request

--- src/components/TicketBookingForm/TicketBookingForm.js
+++ src/components/TicketBookingForm/TicketBookingForm.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { createNewBooking } from "../../api/api";
 class TicketBookingForm extends React.Component {
   constructor() {
     super();
     this.state = {
       customerName: "",
-      customerEmail: ""
+      customerEmail: "",
+      success: false,
+      message: "",
+      submitted: false
     };
   }
 
@@ -18,32 +21,65 @@ class TicketBookingForm extends React.Component {
     });
   };
 
+  onFormSubmit = async event => {
+    try {
+      event.preventDefault();
+      const data = {
+        customerName: this.state.customerName,
+        customerEmail: this.state.customerEmail
+      };
+      await createNewBooking(data);
+      this.setState({
+        customerName: "",
+        customerEmail: "",
+        success: true,
+        message: "Successfully made booking",
+        submitted: true
+      });
+    } catch (err) {
+      this.setState({
+        success: false,
+        message: "Unable to make the booking",
+        submitted: true
+      });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <label>
-          Name
+      <form className="bookingForm" onSubmit={this.onFormSubmit}>
+        <div className="bookingForm__div">
+          <h1 className="bookingForm__heading">Book your Seat</h1>
+          <label>
+            Name
+            <input
+              className="input__text"
+              type="text"
+              aria-label="customer-name"
+              name="customerName"
+              value={this.state.customerName}
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
+          <label>
+            Email
+            <input
+              className="input__text"
+              type="text"
+              aria-label="customer-email"
+              name="customerEmail"
+              value={this.state.customerEmail}
+              onChange={this.handleInputChange}
+            ></input>
+          </label>
           <input
-            className="input__text"
-            type="text"
-            aria-label="customer-name"
-            name="customerName"
-            value={this.state.customerName}
-            onChange={this.handleInputChange}
-          ></input>
-        </label>
-        <label>
-          Email
-          <input
-            className="input__text"
-            type="text"
-            aria-label="customer-email"
-            name="customerEmail"
-            value={this.state.customerEmail}
-            onChange={this.handleInputChange}
-          ></input>
-        </label>
-      </div>
+            className="bookingForm__bookButton"
+            value="Book"
+            type="submit"
+          />
+          {this.state.submitted ? <p>{this.state.message}</p> : ""}
+        </div>
+      </form>
     );
   }
 }
